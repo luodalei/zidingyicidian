f95d738cdd713c794a5ab4bc328265230615c15a
luodalei <47589818+luodalei@users.noreply.github.com>
2019-09-03 18:52:27 +0800

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,94 @@
+/* global api */
+class encn_Bing {
+    constructor(options) {
+        this.options = options;
+        this.maxexample = 2;
+        this.word = '';
+    }
+
+    async displayName() {
+        let locale = await api.locale();
+        if (locale.indexOf('CN') != -1) return '必应英汉词典';
+        if (locale.indexOf('TW') != -1) return '必应英汉词典';
+        return 'Bing CN->EN Dictionary';
+    }
+
+    setOptions(options) {
+        this.options = options;
+        this.maxexample = options.maxexample;
+    }
+
+    async findTerm(word) {
+        this.word = word;
+        return await this.findBing(word);
+    }
+
+    async findBing(word) {
+        if (!word) return null;
+
+        let base = 'https://cn.bing.com/dict/?FORM=HDRSC6';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return null;
+        }
+
+        let content = doc.querySelector('.content') || '';
+        if (!content) return null;
+        let css = this.renderCSS();
+        return css + content.innerHTML;
+    }
+
+    renderCSS() {
+        let css = `
+            <style>
+                .copyright{
+                    display:none;
+                }
+                .orth {
+                    font-size: 100%;
+                    font-weight: bold;
+                }
+                .quote {
+                    font-style: normal;
+                    color: #1683be;
+                }
+                .colloc {
+                    font-style: italic;
+                    font-weight: normal;
+                }
+                .sense {
+                    border: 1px solid;
+                    border-color: #e5e6e9 #dfe0e4 #d0d1d5;
+                    border-radius: 3px;
+                    padding: 5px;
+                    margin-top: 3px;
+                }
+                .sense .re {
+                    font-size: 100%;
+                    margin-left: 0;
+                }
+                .sense .sense {
+                    border: initial;
+                    border-color: initial;
+                    border-radius: initial;
+                    padding: initial;
+                    margin-top: initial;
+                }
+                a {
+                    color: #000;
+                    text-decoration: none;
+                }
+                * {
+                    word-wrap: break-word;
+                    box-sizing: border-box;
+                }
+            </style>`;
+
+        return css;
+    }
+}
